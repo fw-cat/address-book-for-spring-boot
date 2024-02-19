76733e87b6d53b9aad0f3719caa4a264f16af984
miyahara <kenmiya.sns@gmail.com>
2024-02-19 17:39:16 +0900

    [feat]グループの削除機能の追加

--- java/src/main/java/jp/ac/ohara/AddressBook/controller/GroupsController.java
+++ java/src/main/java/jp/ac/ohara/AddressBook/controller/GroupsController.java
@@ -8,28 +8,29 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.ac.ohara.AddressBook.model.AddressGroup;
-import jp.ac.ohara.AddressBook.repository.AddressGroupRepository;
+import jp.ac.ohara.AddressBook.service.AddressGroupService;
 
 @Controller
 @RequestMapping("/groups/")
 public class GroupsController {
 
-	private final AddressGroupRepository groupRepository;
+	private final AddressGroupService groupService;
 
-	public GroupsController(AddressGroupRepository _groupRepository) {
-		this.groupRepository = _groupRepository;
+	public GroupsController(AddressGroupService _groupService) {
+		this.groupService = _groupService;
 	}
 
 	@GetMapping("/")
 	public ModelAndView top(ModelAndView model) {
 		// グループ一覧の取得
-		List<AddressGroup> groupList = this.groupRepository.findAll();
+		List<AddressGroup> groupList = this.groupService.getGroupList();
 
 		model.addObject("groupList", groupList);
 		model.setViewName(this.viewName("top"));
@@ -69,7 +70,7 @@ public class GroupsController {
 	@PostMapping("/complate")
 	public String complate(@ModelAttribute @NonNull AddressGroup addressGroup, RedirectAttributes redirectAttributes) {
 		try {
-			this.groupRepository.save(addressGroup);
+			this.groupService.save(addressGroup);
 			redirectAttributes.addFlashAttribute("exception", "");
 
 		} catch(Exception e) {
@@ -77,9 +78,54 @@ public class GroupsController {
 		}
 		return "redirect:/groups/complate";
 	}
-
 	@GetMapping("/complate")
 	public ModelAndView cmplateRedirect(@ModelAttribute("exception") String exception, ModelAndView model) {
+		model.addObject("exception", exception);
+		model.setViewName(this.viewName("complate"));
+		return model;
+	}
+
+	@GetMapping("/edit/{groupId}")
+	public ModelAndView edit(@PathVariable @NonNull Long groupId, ModelAndView model) {
+		AddressGroup addressGroup = this.groupService.get(groupId);
+		model.addObject("addressGroup", addressGroup);
+
+		// Viewの設定
+		model.addObject("method", "編集");
+		model.addObject("action", "confirm");
+		model.addObject("button", "確認");
+		model.setViewName(this.viewName("form"));
+
+		return model;
+	}
+
+	@GetMapping("/delete/{groupId}")
+	public ModelAndView deleteConfirm(@PathVariable @NonNull Long groupId, ModelAndView model) {
+		AddressGroup addressGroup = this.groupService.get(groupId);
+		model.addObject("addressGroup", addressGroup);
+
+		// Viewの設定
+		model.addObject("method", "削除確認");
+		model.addObject("action", "delete/" + groupId);
+		model.addObject("button", "削除");
+		model.setViewName(this.viewName("delete_confirm"));
+
+		return model;
+	}
+	@PostMapping("/delete/{groupId}")
+	public String delete(RedirectAttributes redirectAttributes, @PathVariable @NonNull Long groupId, ModelAndView model) {
+		try {
+			this.groupService.delete(groupId);
+			redirectAttributes.addFlashAttribute("exception", "");
+
+		} catch(Exception e) {
+			redirectAttributes.addFlashAttribute("exception", e.getMessage());
+		}
+		return "redirect:/groups/delete/complate";
+	}
+	@GetMapping("/delete/complate")
+	public ModelAndView deleteComp(@ModelAttribute("exception") String exception, ModelAndView model) {
+		model.addObject("method", "削除");
 		model.addObject("exception", exception);
 		model.setViewName(this.viewName("complate"));
 		return model;
